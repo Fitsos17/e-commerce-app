b807cf169e0f6a4806688652d52f6afd85051729
Nick <Nikolas.amiridis@gmail.com>
2022-04-20 22:22:48 +0300

    Implemented react context to our project and added sign out and onAuthStateChanged observer

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -22,18 +22,16 @@ const SignInForm = () => {
   };
 
   const signInWithGoogle = async () => {
-    const { user } = await signInWithGooglePopup();
-    await createUserDocumentFromAuth(user);
+    await signInWithGooglePopup();
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const response = await signInAuthUserWithEmailAndPassword(
+      const { user } = await signInAuthUserWithEmailAndPassword(
         email,
         password
       );
-      console.log(response);
       resetFormFields();
     } catch (err) {
       switch (err.code) {
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -7,6 +7,8 @@ import {
   createUserWithEmailAndPassword,
   signInWithPopup,
   signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
 } from "firebase/auth";
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
@@ -31,13 +33,24 @@ googleProvider.setCustomParameters({
 
 export const auth = getAuth();
 
+// sign in with google Pop Up
 export const signInWithGooglePopup = () =>
   signInWithPopup(auth, googleProvider);
+
+// sign in with google redirect
 export const signInWithGoogleRedirect = () =>
   signInWithRedirect(auth, googleProvider);
 
+// sign in user with email and password
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await signInWithEmailAndPassword(auth, email, password);
+};
+
 export const db = getFirestore();
 
+// create user and store it to firestore
 export const createUserDocumentFromAuth = async (
   userAuth,
   additionalInformation = {}
@@ -68,13 +81,14 @@ export const createUserDocumentFromAuth = async (
   return userDocRef;
 };
 
+// create user
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
   if (!email || !password) return;
   return await createUserWithEmailAndPassword(auth, email, password);
 };
 
-export const signInAuthUserWithEmailAndPassword = async (email, password) => {
-  if (!email || !password) return;
+// sign out user
+export const signOutUser = async () => await signOut(auth);
 
-  return await signInWithEmailAndPassword(auth, email, password);
-};
+export const onAuthStateChangedListener = (callback) =>
+  onAuthStateChanged(auth, callback);
