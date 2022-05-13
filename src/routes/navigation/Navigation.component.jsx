d6eb5d8863adf4f3e8960309f669efda4337db81
Nick <Nikolas.amiridis@gmail.com>
2022-05-13 22:36:39 +0300

    added some spacing

--- src/routes/navigation/Navigation.component.jsx
+++ src/routes/navigation/Navigation.component.jsx
@@ -1,11 +1,15 @@
-import React, { useContext } from "react";
+import { Fragment, useContext } from "react";
 import { Outlet } from "react-router-dom";
-import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
-import { UserContext } from "../../contexts/user.context";
-import { CartContext } from "../../contexts/cart.context";
-import { signOutUser } from "../../utils/firebase/firebase.utils";
+
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+
+import { UserContext } from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.context";
+
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import { signOutUser } from "../../utils/firebase/firebase.utils";
+
 import {
   NavigationContainer,
   NavLinks,
@@ -18,26 +22,27 @@ const Navigation = () => {
   const { isCartOpen } = useContext(CartContext);
 
   return (
-    <React.Fragment>
-      <NavigationContainer className="navigation">
+    <Fragment>
+      <NavigationContainer>
         <LogoContainer to="/">
           <CrwnLogo className="logo" />
         </LogoContainer>
         <NavLinks>
           <NavLink to="/shop">SHOP</NavLink>
+
           {currentUser ? (
             <NavLink as="span" onClick={signOutUser}>
               SIGN OUT
             </NavLink>
           ) : (
-            <NavLink to="/auth">SIGN-IN</NavLink>
+            <NavLink to="/auth">SIGN IN</NavLink>
           )}
           <CartIcon />
         </NavLinks>
         {isCartOpen && <CartDropdown />}
       </NavigationContainer>
       <Outlet />
-    </React.Fragment>
+    </Fragment>
   );
 };
 
